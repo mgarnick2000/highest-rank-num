9f8d7f7683b00ca86f6e354357f29b694601fd5d
Mark <mgarnick@techtonicgroup.com>
2019-09-25 10:31:00 -0600

    able to resolve one issue, still need to get the greatest mode

--- Program.cs
+++ Program.cs
@@ -18,20 +18,28 @@ namespace highest_rank_num
             // var arr = new int[] { 12, 10, 8, 12, 7, 6, 4, 10, 12, 10, 12, 10, 8, 12, 7, 6, 4, 10, 10,12, 12, 10, 8, 12, 7, 6, 4, 10, 12, 10, 10, 10, 12, 8, 12, 7, 6, 4, 10, 12 };
             // var arr = new int[] { 5, 3, 5, 6, 4, 11, 10, 3, 4, 10, 4, 5, 4, 8, 6, 6, 4, 4, 6, 6, 5, 3, 6, 4, 6, 5, 7, 10, 9, 5, 8, 7, 5, 10, 7, 6, 8, 4, 9, 3, 10, 7, 8, 3, 9, 3, 5, 10, 8, 6, 10, 10, 8, 6, 10, 6, 10, 9, 3, 7, 3, 8, 4, 4, 11, 8, 9, 10 };
             var arr = new int[] { 10, 12, 8, 12, 7, 6, 4, 10, 12, 10 };
+            // var arr = new int[] { 9, 10, 4, 3, 9, 3, 11, 11, 5, 9, 9 };
             // var arr = new int[] { 8, 11 };
             HighestRank(arr);
         }
 
         public static int HighestRank(int[] arr)
         {
-            int mode = arr.GroupBy(v => v)
-            .OrderByDescending(g => g.Count())
-            .First()
-            .Key;
+            
+            var groups = arr.GroupBy(v => v);
+            int maxCount = groups.Max(g => g.Count());
+            int mode = groups.First(g => g.Count() == maxCount).Key;
+
+            int high = arr.OrderByDescending(i => i).First();
+
+            if (maxCount == 1)
+            {
+                WriteLine(high);
+                return high;
+            }
+
 
             WriteLine(mode);
-
-
             return mode;
         }
 
