a164d2f1a0fe888bb24ebf25fe4311e2d5a249e7
Mark <mgarnick@techtonicgroup.com>
2019-09-25 08:18:24 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace highest_rank_num
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
